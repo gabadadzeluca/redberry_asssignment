d418bd44ea287e2f03315f0d22b1249f2f1a8033
Luca <gabadadzeluca@gmail.com>
2023-02-04 22:52:15 +0400

    add email and number validation

--- js/main.js
+++ js/main.js
@@ -2,15 +2,23 @@
 
 let currentForm = 1;
 const patternGeo = /^[ა-ჰ]+$/;
+const patterEmail = /^[a-zA-Z0-9._%+-]+@redberry\.ge$/;
+const patternNumber = /^(\+995\s?5|5)\s?(\d{3}\s?){2}\d{2}$/;
 
-
+const formPrivate = document.querySelector('.form-private');
 const textInputs = document.querySelectorAll('input[type="text"]');
+const emailInput = document.getElementById('email');
+const numberInput = document.getElementById('mobile');
 
-document.querySelector('.form-private').addEventListener('input', function(event){
+formPrivate.addEventListener('input', function(event){
     textInputs.forEach(input=>{
         checkTextValidity(input);
     });
+    checkEmailValidity(emailInput);
+    checkNumberValidity(numberInput);
 });
+
+
 
 
 function checkTextValidity(input){
@@ -25,6 +33,25 @@ function checkTextValidity(input){
     }
 }
 
+function checkEmailValidity(input){
+    if(!patterEmail.test(input.value)){
+        displayInvalidInput(input);
+        showError(input);
+    }else{
+        displayValidInput(input);
+        hideError(input);
+    }
+}
+
+function checkNumberValidity(input){
+    if(!patternNumber.test(input.value)){
+        displayInvalidInput(input);
+        showError(input);
+    }else{
+        displayValidInput(input);
+        hideError(input);
+    }
+}
 
 function showError(input) {
     const parent = input.parentElement;
