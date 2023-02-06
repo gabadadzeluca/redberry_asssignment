2217f092b3a17a3dc7090bdd0a0e1f243f6ea944
Luca <gabadadzeluca@gmail.com>
2023-02-06 23:43:33 +0400

    add js file to store error messages

---
+++ js/errors.js
@@ -0,0 +1,8 @@
+const textError = 'მინიმუმ 2 ასო';
+const textErrorName = 'მინიმუმ 2 ასო, ქართული ასოები';
+const telError = 'უნდა ქართული მობილურის ნომრის ფორმატს';
+const emailError = 'უნდა მთავრდებოდეს @redberry.ge-ით';
+const fileError = 'ფაილის ზომა ლიმიტს აღემატება';
+
+
+export {textError, textErrorName, telError, emailError, fileError};
