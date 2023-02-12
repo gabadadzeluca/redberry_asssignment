61f97c8d1409939fec6d8dc44337dbfa215dfb95
Luca <gabadadzeluca@gmail.com>
2023-02-12 17:57:04 +0400

    add formatData function, image to blob convertor

--- js/requests.js
+++ js/requests.js
@@ -4,12 +4,24 @@ const formsExp = JSON.parse(localStorage.getItem('formsExp'));
 const formsEdu = JSON.parse(localStorage.getItem('formsEdu'));
 const formPrivate = JSON.parse(localStorage.getItem('formPrivate'));
 
+
+let binaryData;
+function base64toBlob(){
+    const base64EncodedString = JSON.parse(localStorage.getItem('imageData')).split(',')[1];
+    binaryData = atob(base64EncodedString);
+}
+base64toBlob();
+
+const blob = new Blob([binaryData], { type: 'image/jpeg' });
+
+
+let experiences = [];
+let educations = [];
 const url = 'https://resume.redberryinternship.ge/api/cvs';
 formatData(formsEdu);
 formatData(formsExp);
 
 function formatData(formsArray){
-    
     for (let i = 0; i < formsArray.length; i++) {
         const original = formsArray[i];
         const data = original.data;
@@ -22,15 +34,52 @@ function formatData(formsArray){
                 else if(key.includes('grad-describtion')) data['description'] = data[key];
                 else if(key.includes('degree')) data['degree_id'] = data[key];
             }else if(formsArray == formsExp){
-                if(key.includes('position')) continue; 
-                else if(key.includes('employer')) continue;
-                if(key.includes('start-date')) data['start_date'] = data[key];
+                if(key == 'position') continue; 
+                else if(key == 'employer') continue;
+                else if(key.includes('position')) data['position'] = data[key];
+                else if(key.includes('employer')) data['employer'] = data[key];
+                else if(key.includes('start-date')) data['start_date'] = data[key];
                 else if(key.includes('describtion')) data['description'] = data[key];
             }
             delete data[key];
         }
+        if(formsArray == formsEdu){
+            educations.push(data);
+        }else if(formsArray == formsExp){
+            experiences.push(data);
+        }
     }
-    console.log(formsArray);
 }
+
+formatPrivate(formPrivate);
+
+function formatPrivate(formPrivate){
+    const imageData = JSON.parse(localStorage.getItem('imageData'));
+    const base64Image = imageData.split(',')[1];
+    formPrivate['phone_number'] = formPrivate['mobile'];
+    delete formPrivate['mobile'];
+    formPrivate['about_me'] = formPrivate['about-user'];
+    delete formPrivate['about-user'];
+    formPrivate['image'] = blob;
+    console.log(formPrivate);
+}
+
+
+const dataToSend = {
+    "name": formPrivate.name,
+    "surname": formPrivate.surname,
+    "email": formPrivate.email,
+    "phone_number": formPrivate.phone_number,
+    "experiences": experiences,
+    "educations": educations,
+    "image": formPrivate.image,
+    "about_me": formPrivate.about_me
+}
+
+console.log(dataToSend);
+
+
+
+
 
 
