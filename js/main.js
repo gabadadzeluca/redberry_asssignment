fe4029bc28429b71c2f6342a671fa6152d144a82
Luca <gabadadzeluca@gmail.com>
2023-02-12 21:04:09 +0400

    import functions from functional.js

--- js/main.js
+++ js/main.js
@@ -4,11 +4,12 @@ import {getDegrees} from "./degrees.js";
 
 const degreesList = await getDegrees();
 let currentForm = 1;
-const patternGeoName = /^[ა-ჰ]+$/u;
-const patternGeo = /^[ა-ჰ\s]+$/;
 
-const patternEmail = /^[a-zA-Z0-9._%+-]+@redberry\.ge$/;
-const patternNumber = /^(\+995\s?5|5)\s?(\d{3}\s?){2}\d{2}$/; 
+import {
+    displayInvalidInput, displayValidInput, hideError, showError, 
+    checkDateValidity, checkSelect, checkTextValidity, checkEmailValidity,
+    checkTextarea, checkNumberValidity, patternEmail, patternGeo, patternNumber, patternGeoName
+} from "./functional.js";
 
 const formPrivateDiv = document.querySelector('.form-private');
 
@@ -155,7 +156,6 @@ function handleForm(form){
         validationArray = [textsValid, datesValid, textAreaValid, selectValid];
     }
     formIsValid = validationArray.every(value=>{
-        console.log(value);
         return value === true;
     });
     console.log(form.id,"valid:",formIsValid);
@@ -167,11 +167,6 @@ function handleForm(form){
         prevBtn.addEventListener('click', prevForm);
     }
     
-    if(textArea && textArea.dataset.mandatory == true){
-        console.log('mandatory', textArea.dataset.mandatory);
-        checkTextarea(textArea);
-    }
-
 }
 
 
@@ -182,7 +177,6 @@ if(JSON.parse(localStorage.getItem('formsExp')).length > 0){
 if(JSON.parse(localStorage.getItem('formsEdu')).length > 0){
     displayData(formEdu);
 }
-// add checking for local storage
 
 
 function displayData(form){ 
@@ -281,113 +275,6 @@ function saveImage(file) { // save image as base64
     reader.readAsDataURL(file);
 }
   
-  
-
-function checkTextValidity(input){
-    if(input.name.includes('name') || input.name.includes('surname')){
-        if (! patternGeoName.test(input.value) || input.value.length < 2) {
-            displayInvalidInput(input);
-            showError(input);
-        }else{
-            displayValidInput(input);
-            hideError(input);
-            return true;
-        }
-    }else{
-        if(!patternGeo.test(input.value) || input.value.length < 2){   
-            displayInvalidInput(input);
-            showError(input);
-        }else{
-            displayValidInput(input);
-            hideError(input);
-            return true;
-        }
-    }
-}
-
-function checkEmailValidity(input){
-    if(!patternEmail.test(input.value)){
-        displayInvalidInput(input);
-        showError(input);
-    }else{
-        displayValidInput(input);
-        hideError(input);
-        return true;
-    }
-}
-
-function checkNumberValidity(input){
-    if(!patternNumber.test(input.value.trim())){
-        displayInvalidInput(input);
-        showError(input);
-    }else{
-        displayValidInput(input);
-        hideError(input);
-        return true;
-    }
-}
-
-function checkDateValidity(input){
-    if(input.value !== "") {
-        displayValidInput(input);
-        return isValidDate(input.value);
-    }else{
-        displayInvalidInput(input);
-        return false;
-    }
-}
-
-function isValidDate(dateString) {
-    let date = new Date(dateString);
-    return !isNaN(date.getTime());
-}
-
-function checkTextarea(textarea){
-    
-    if(textarea.getAttribute('data-mandatory') == 'true'){
-        if(patternGeo.test(textarea.value) && textarea.value.trim().length > 2){
-            displayValidInput(textarea);
-            return true;
-        }else{
-            displayInvalidInput(textarea);
-            return false;
-        }
-    }
-}
-
-function checkSelect(select){
-    if(select.value !== "default"){
-        displayValidInput(select);
-        return true;
-    }else{
-        displayInvalidInput(select);
-    }
-
-}
-
-function showError(input) {
-    const parent = input.parentElement;
-    const error = parent.querySelector('.error');
-    error.style.display = 'inline';
-}
-function hideError(input){
-    const parent = input.parentElement;
-    parent.querySelector('.error').style.display = 'none';
-}
-
-function displayInvalidInput(input){
-    input.classList.forEach(className=>{
-        input.classList.remove(className);
-    });
-    input.classList.add('invalid');
-}
-function displayValidInput(input){
-    input.classList.forEach(className=>{
-        input.classList.remove(className);
-    });
-    input.classList.add('valid')
-}
-
 function displayDegrees(){
     let degreeMenu = document.getElementById('degree-menu');
     let options = '<option disabled selected value="default">აირჩიეთ ხარისხი</option>';
